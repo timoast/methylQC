e1bb5547580cb4c2a9538cc02942bd816e1d0ab4
timoast <timstuart90@gmail.com>
2016-09-01 17:20:29 +0800

    Add strandbias function
    Calculates bias in sequencing depth towards either strand for each C position

---
+++ R/strandBias.R
@@ -0,0 +1,51 @@
+#' Strand sequencing bias
+#'
+#' This function will calculate the strand depth bias for each C base position
+#' @param data input dataframe
+#' @param window size of sliding window. Default is 30 bases.
+#' @param fill Value used to fill in where there isn't enough data for window size. Default is 0.
+#' @export
+#' @return a dataframe
+#' @examples
+#' strandBias(data)
+strandBias <- function(data, window = 30, fill = 0) {
+  data$depth <- ifelse(data$base == "G", -(data$depth), data$depth)
+  
+  # split into different dataframes for each strand
+  wat <- dplyr::filter(data, base == "C")
+  cri <- dplyr::filter(data, base == "G")
+  
+  # fill in position information
+  filled_wat <- fillBases(wat)
+  filled_cri <- fillBases(cri)
+
+  # calculate rolling mean for every position
+  filled_wat$meanCovC <- zoo::rollapply(filled_wat$depth, width = window, mean, fill = fill, na.rm = T)
+  filled_wat <- dplyr::select(filled_wat, position, meanCovC)
+  filled_cri$meanCovG <- zoo::rollapply(filled_cri$depth, width = window, mean, fill = fill, na.rm = T)
+  filled_cri <- dplyr::select(filled_cri, position, meanCovG)
+  
+  # Convert NaN to NA
+  filled_wat$meanCovC[is.nan(filled_wat$meanCovC)] <- NA
+  filled_cri$meanCovG[is.nan(filled_cri$meanCovG)] <- NA
+  
+  # intersect with original data
+  data <- dplyr::left_join(data, filled_wat)
+  data <- dplyr::left_join(data, filled_cri)
+
+  # calculate bias at each position
+  data <- dplyr::rowwise(data)
+  data <- dplyr::mutate(data, strandBias = (meanCovC + meanCovG))
+  data <- dplyr::select(data, -(meanCovC:meanCovG))
+
+  return(data)
+}
+
+fillBases <- function(d) {
+  mn <- min(d$position, na.rm = T)
+  mx <- max(d$position, na.rm = T)
+  newdf <- data.frame(position = seq(from = mn, to = mx), stringsAsFactors = F)
+  n <- dplyr::full_join(d, newdf)
+  n <- dplyr::arrange(n, position)
+  return(n)
+}
